fbd4cfe0cb272bac0e26c93d0396edfca8664868
Akinori Sakakibara <gorion.1213@gmail.com>
2020-07-25 01:25:17 +0900

    initial commit for React

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from '../Button/Button';
+
+const Home = ({history}) => {
+  console.log(history, '@@@@@@')
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button
+       onClickHandler={() => {
+          history.push('/quiz');
+        }}
+      >
+        クイズページへ移動
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
